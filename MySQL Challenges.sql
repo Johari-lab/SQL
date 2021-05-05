4f1f5bc097787b7edd2ba89c5fba8c908697ca5c
María Campaña <71124586+Johari-lab@users.noreply.github.com>
2021-05-05 11:01:11 +0200

    Update MySQL Challenges.sql

--- MySQL Challenges.sql
+++ MySQL Challenges.sql
@@ -1,5 +1,4 @@
-/*Start by building the schema and populating your MySQL database sandbox 
-with the following:*/
+/*Start by building the schema and populating your MySQL database sandbox with the following:*/
 CREATE TABLE rating (
     id INT,
     app_name VARCHAR(255),
@@ -18,3 +17,23 @@ VALUES (32165, "Centipede", 99632342, 5.99, 3.7, "Games");
 
 INSERT INTO rating (id, app_name, app_size, price, total_ratings, genre) 
 VALUES (6549873, "Dracula", 10243, 1.99, 4.9, "Books");
+
+/*QUERIES:*/
+
+/*Let's see our table*/
+SELECT *
+FROM rating;
+
+/*TASKS:*/
+/*1. Find the sum of all of the prices of all of the applications within the rating table*/
+SELECT SUM(price)
+FROM rating;
+
+/*2. Find the maximum value of total_ratings of all of the applications within the rating table*/
+SELECT MAX(total_ratings)
+FROM rating;
+
+/*3. Find the average prices grouped by genre within the rating table*/
+SELECT genre, AVG(price)
+FROM rating
+GROUP BY genre;
